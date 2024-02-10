9acd6c5179dbd92c945ca376a1fa827d148970cb
Ashley Bennett <ashleyben21@gmail.com>
2024-02-10 16:44:28 +0000

    added sorting functionality

--- backend/number_sorter/number_sorter/Controllers/NumbersController.cs
+++ backend/number_sorter/number_sorter/Controllers/NumbersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 using System.Data;
+using System.Diagnostics;
 
 namespace number_sorter.Controllers
 {
@@ -41,8 +42,33 @@ namespace number_sorter.Controllers
 
         [HttpPost]
         [Route("AddNumbers")]
-        public JsonResult AddNumbers([FromForm] string newNumbers, bool ascending)
+        public JsonResult AddNumbers([FromForm] string addedNumbers, bool ascending)
         {
+            //stopwatch for operations
+            Stopwatch sw;
+            sw = Stopwatch.StartNew();
+            string[] separatedNumbers = addedNumbers.Split(' ');
+            //convert string array to int array
+            int[] separatedNumbersInts = Array.ConvertAll(separatedNumbers, int.Parse);
+
+            int[] sorted = new int[separatedNumbersInts.Length];
+            for (int i = 0; i < separatedNumbersInts.Length; i++)
+            {
+                sorted[i] = separatedNumbersInts[i];
+            }
+            Array.Sort(sorted);
+
+            // reverse array for descending
+            if (!ascending)
+            {
+                Array.Reverse(sorted);
+            }
+
+            //int array to strings
+            string[] result = sorted.Select(x => x.ToString()).ToArray();
+            string sortedNumbers = string.Join(" ", result);
+            sw.Stop();
+
             string query = "insert into numbers values(@newNumbers, @ascending, @sortTime)";
             DataTable dt = new DataTable();
             string sqlDatasource = _configuration.GetConnectionString("numberSorterDBCon");
@@ -52,9 +78,9 @@ namespace number_sorter.Controllers
                 myConn.Open();
                 using (SqlCommand myCommand = new SqlCommand(query, myConn))
                 {
-                    myCommand.Parameters.AddWithValue("@newNumbers", newNumbers);
+                    myCommand.Parameters.AddWithValue("@newNumbers", sortedNumbers);
                     myCommand.Parameters.AddWithValue("@ascending", ascending);
-                    myCommand.Parameters.AddWithValue("@sortTime", 12);
+                    myCommand.Parameters.AddWithValue("@sortTime", sw.ElapsedMilliseconds);
 
 
                     myReader = myCommand.ExecuteReader();
