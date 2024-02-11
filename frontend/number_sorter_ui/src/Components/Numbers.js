6eacc4cf8664a936991d228fcf271ab88aa4d1be
Ashley Bennett <ashleyben21@gmail.com>
2024-02-11 14:36:16 +0000

    styling and validation

--- frontend/number_sorter_ui/src/Components/Numbers.js
+++ frontend/number_sorter_ui/src/Components/Numbers.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 
 const Numbers = () => {
   const API_URL = "http://localhost:20954";
+  const inputValidationRegex = /^(?!.* {2})[0-9 ]*(?<! )$/gm;
 
   const getAllNumbers = () => {
     fetch(API_URL + "/api/Numbers/GetNumbers")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setnumbersSorted(data);
       })
       .catch((e) => {
@@ -18,22 +18,31 @@ const Numbers = () => {
   const handleSubmitNumbers = (e) => {
     e.preventDefault();
     setNumberValidationMessage("");
-
-    // Read the form data
-    const form = e.target;
-    const formData = new FormData(form);
-    console.log(formData.getAll("addedNumbers"));
-
-    const re = /^(?!.* {2})[0-9 ]*$/gm;
-    const found = formData.getAll("addedNumbers")[0].match(re);
-
-    console.log(found, " match");
-
-    if (found === null) {
-      setNumberValidationMessage("You must only uses numbers and spaces");
+    setInputValidation("");
+    setSubmitButtonDisabled(true);
+    if (inputValidation !== "valid") {
+      setNumberValidationMessage(
+        "Input not valid, check your numbers and try again"
+      );
+      setSubmitButtonDisabled(false);
       return;
     }
 
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const addedNumbers = formData.getAll("addedNumbers")[0];
+    const match = addedNumbers.match(inputValidationRegex);
+
+    if (match === null) {
+      setNumberValidationMessage(
+        "You must only uses numbers and single spaces"
+      );
+      return;
+    } else if (match[0] === "") {
+      setNumberValidationMessage("This cannot be empty");
+      return;
+    }
     fetch(
       API_URL +
         `/api/Numbers/AddNumbers?ascending=${formData.getAll("ascending")[0]}`,
@@ -46,22 +55,51 @@ const Numbers = () => {
       .then((data) => {
         console.log(data);
         setValidationMessage(data.SuccessResult);
+        setInputValidation("valid");
+        setSubmitButtonDisabled(false);
       });
+  };
+
+  const checkValidation = (e) => {
+    e.preventDefault();
+    const input = e.target.value;
+    const match = input.match(inputValidationRegex);
+
+    if (match === null) {
+      setNumberValidationMessage(
+        "You must only uses numbers and single spaces"
+      );
+      setInputValidation("invalid");
+      return;
+    } else if (match[0] === "") {
+      setNumberValidationMessage("");
+      setInputValidation("");
+      return;
+    } else {
+      setNumberValidationMessage("");
+      setInputValidation("valid");
+    }
   };
 
   const [numbersSorted, setnumbersSorted] = useState([]);
   const [validationMessage, setValidationMessage] = useState("");
   const [numberValidationMessage, setNumberValidationMessage] = useState("");
+  const [inputValidation, setInputValidation] = useState("");
+  const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
   return (
     <div>
-      {numberValidationMessage}
       <form method="post" onSubmit={handleSubmitNumbers}>
-        <label>
-          Enter space separated series of numbers: <input name="addedNumbers" />
+        Enter single space separated series of numbers:{" "}
+        <label className="inputControl" onInput={checkValidation}>
+          <input
+            name="addedNumbers"
+            className={`inputNumbers ${inputValidation}`}
+          />{" "}
         </label>
+        {numberValidationMessage}
         <p>
           Order:
-          <label>
+          <label class="radioControl">
             <input
               type="radio"
               name="ascending"
@@ -70,33 +108,48 @@ const Numbers = () => {
             />
             Ascending
           </label>
-          <label>
+          <label class="radioControl">
             <input type="radio" name="ascending" value={false} />
             Descending
           </label>
         </p>
-        <button type="submit">Sort</button>
+        <button
+          className="buttonMain"
+          type="submit"
+          disabled={submitButtonDisabled}
+        >
+          Sort
+        </button>
       </form>
-      <p>{validationMessage}</p>
-      <button onClick={getAllNumbers}>Get numbers</button>
-      {numbersSorted.length > 0 ? (
-        <table>
-          <tr>
-            <th>Numbers</th>
-            <th>Order</th>
-          </tr>
-          {numbersSorted.map((numberSequence) => {
-            return (
-              <tr>
-                <td>{numberSequence.numbers}</td>
-                <td>{numberSequence.ascending ? "Ascending" : "Descending"}</td>
-              </tr>
-            );
-          })}
-        </table>
-      ) : (
-        <></>
-      )}
+      <div className="result">
+        <p>{validationMessage}</p>
+        <p>These are your numbers sorted {}:</p>
+      </div>
+      <div className="tableContainer">
+        <button onClick={getAllNumbers} className="buttonMain">
+          Get numbers
+        </button>
+        {numbersSorted.length > 0 ? (
+          <table>
+            <tr>
+              <th>Numbers</th>
+              <th>Order</th>
+            </tr>
+            {numbersSorted.map((numberSequence) => {
+              return (
+                <tr>
+                  <td>{numberSequence.numbers}</td>
+                  <td>
+                    {numberSequence.ascending ? "Ascending" : "Descending"}
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
