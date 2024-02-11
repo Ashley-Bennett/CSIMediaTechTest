0e282ec25892d5f924006b33a6927e0d7ab1b162
Ashley Bennett <ashleyben21@gmail.com>
2024-02-11 13:34:13 +0000

    validation to input

--- frontend/number_sorter_ui/src/Components/Numbers.js
+++ frontend/number_sorter_ui/src/Components/Numbers.js
@@ -2,22 +2,37 @@ import React, { useState } from "react";
 
 const Numbers = () => {
   const API_URL = "http://localhost:20954";
+
   const getAllNumbers = () => {
     fetch(API_URL + "/api/Numbers/GetNumbers")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setnumbersSorted(data);
+      })
+      .catch((e) => {
+        console.log(e);
       });
   };
 
   const handleSubmitNumbers = (e) => {
     e.preventDefault();
+    setNumberValidationMessage("");
 
     // Read the form data
     const form = e.target;
     const formData = new FormData(form);
-    console.log(formData.getAll("ascending"));
+    console.log(formData.getAll("addedNumbers"));
+
+    const re = /^(?!.* {2})[0-9 ]*$/gm;
+    const found = formData.getAll("addedNumbers")[0].match(re);
+
+    console.log(found, " match");
+
+    if (found === null) {
+      setNumberValidationMessage("You must only uses numbers and spaces");
+      return;
+    }
 
     fetch(
       API_URL +
@@ -36,8 +51,10 @@ const Numbers = () => {
 
   const [numbersSorted, setnumbersSorted] = useState([]);
   const [validationMessage, setValidationMessage] = useState("");
+  const [numberValidationMessage, setNumberValidationMessage] = useState("");
   return (
     <div>
+      {numberValidationMessage}
       <form method="post" onSubmit={handleSubmitNumbers}>
         <label>
           Enter space separated series of numbers: <input name="addedNumbers" />
@@ -62,20 +79,24 @@ const Numbers = () => {
       </form>
       <p>{validationMessage}</p>
       <button onClick={getAllNumbers}>Get numbers</button>
-      <table>
-        <tr>
-          <th>Numbers</th>
-          <th>Order</th>
-        </tr>
-        {numbersSorted.map((numberSequence) => {
-          return (
-            <tr>
-              <td>{numberSequence.numbers}</td>
-              <td>{numberSequence.ascending ? "Ascending" : "Descending"}</td>
-            </tr>
-          );
-        })}
-      </table>
+      {numbersSorted.length > 0 ? (
+        <table>
+          <tr>
+            <th>Numbers</th>
+            <th>Order</th>
+          </tr>
+          {numbersSorted.map((numberSequence) => {
+            return (
+              <tr>
+                <td>{numberSequence.numbers}</td>
+                <td>{numberSequence.ascending ? "Ascending" : "Descending"}</td>
+              </tr>
+            );
+          })}
+        </table>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
